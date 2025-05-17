1eb6955c2a40e7f68889707aac8345150c08f07a
happy-3 <142456934+happy-3@users.noreply.github.com>
2025-05-18 07:34:48 +0900

    Break up preview log initializer to aid Swift compiler

--- GPS Logger/DistanceGraphView.swift
+++ GPS Logger/DistanceGraphView.swift
@@ -51,26 +51,32 @@ struct DistanceGraphView_Previews: PreviewProvider {
                                               endLocation: .init(latitude: 0, longitude: 0),
                                               horizontalDistance: 100,
                                               totalDistance: 110)
-        let logs: [FlightLog] = (0..<10).map { i in
-            FlightLog(timestamp: Date().addingTimeInterval(Double(i)*6),
-                      latitude: 0,
-                      longitude: 0,
-                      gpsAltitude: Double(1000 + i*10),
-                      speedKt: 0,
-                      magneticCourse: 0,
-                      horizontalAccuracyM: 5,
-                      verticalAccuracyFt: 10,
-                      altimeterPressure: nil,
-                      rawGpsAltitudeChangeRate: 0,
-                      relativeAltitude: 0,
-                      barometricAltitude: Double(1000 + i*10),
-                      latestAcceleration: 0,
-                      fusedAltitude: Double(1000 + i*8),
-                      fusedAltitudeChangeRate: 0,
-                      baselineAltitude: nil,
-                      measuredAltitude: nil,
-                      kalmanUpdateInterval: nil,
-                      photoIndex: nil)
+        let logs: [FlightLog] = (0..<10).map { i -> FlightLog in
+            let timestamp = Date().addingTimeInterval(Double(i) * 6)
+            let gpsAlt = Double(1000 + i * 10)
+            let fusedAlt = Double(1000 + i * 8)
+
+            return FlightLog(
+                timestamp: timestamp,
+                latitude: 0,
+                longitude: 0,
+                gpsAltitude: gpsAlt,
+                speedKt: 0,
+                magneticCourse: 0,
+                horizontalAccuracyM: 5,
+                verticalAccuracyFt: 10,
+                altimeterPressure: nil,
+                rawGpsAltitudeChangeRate: 0,
+                relativeAltitude: 0,
+                barometricAltitude: gpsAlt,
+                latestAcceleration: 0,
+                fusedAltitude: fusedAlt,
+                fusedAltitudeChangeRate: 0,
+                baselineAltitude: nil,
+                measuredAltitude: nil,
+                kalmanUpdateInterval: nil,
+                photoIndex: nil
+            )
         }
         NavigationView {
             DistanceGraphView(logs: logs, measurement: measurement)
