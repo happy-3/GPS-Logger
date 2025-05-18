66a20e3406a4a7a7c9294bf82ac6941319c4971a
happy-3 <142456934+happy-3@users.noreply.github.com>
2025-05-18 16:39:03 +0900

    Refactor Settings with UserDefaults-backed wrapper

---
+++ GPS Logger/UserDefaultBacked.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Combine
+
+/// A property wrapper that synchronizes a value with UserDefaults and publishes changes.
+@propertyWrapper
+struct UserDefaultBacked<Value> {
+    private let key: String
+    private let defaultValue: Value
+    @Published private var value: Value
+
+    var wrappedValue: Value {
+        get { value }
+        set {
+            value = newValue
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+
+    var projectedValue: Published<Value>.Publisher { $value }
+
+    init(wrappedValue defaultValue: Value, key: String) {
+        self.key = key
+        self.defaultValue = defaultValue
+        let stored = UserDefaults.standard.object(forKey: key) as? Value ?? defaultValue
+        self._value = Published(initialValue: stored)
+    }
+}
