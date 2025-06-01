955e415eeab5b1c4424f2007f77e9148bdfee4e3
happy-3 <142456934+happy-3@users.noreply.github.com>
2025-06-01 16:12:40 +0900

    Add Swift Package manifest

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "GPSLogger",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(name: "GPS_Logger", targets: ["GPS_Logger"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-testing.git", from: "0.3.0")
+    ],
+    targets: [
+        .target(
+            name: "GPS_Logger",
+            path: "GPS Logger",
+            resources: [
+                .process("Assets.xcassets"),
+                .process("Preview Content")
+            ]
+        ),
+        .testTarget(
+            name: "GPS_LoggerTests",
+            dependencies: [
+                "GPS_Logger",
+                .product(name: "Testing", package: "swift-testing")
+            ],
+            path: "GPS LoggerTests"
+        )
+        // UI tests are not included in SPM
+    ]
+)
